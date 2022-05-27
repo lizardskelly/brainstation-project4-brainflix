a237a878cbffefa0a96b04c76ccfe1531e67d0ac
Liz Meeker <lizmeeker90@gmail.com>
2022-05-27 00:14:36 -0400

    added player styling

--- src/components/VideoPlayer/VideoPlayer.js
+++ src/components/VideoPlayer/VideoPlayer.js
@@ -1,9 +1,12 @@
+import './videoplayer.scss'
+
 const VideoPlayer = ({ video }) => {
   const { image } = video;
 
   return ( 
     <>
       <video
+        className='video'
         controls
         poster={image}
       ></video>
