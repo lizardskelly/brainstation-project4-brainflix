bb930e6e3e3fccdbedddce32401d3b7a7966f9d6
Liz Meeker <lizmeeker90@gmail.com>
2022-06-05 14:13:31 -0400

    added upload page desktop styling

--- src/pages/UploadPage/UploadPage.js
+++ src/pages/UploadPage/UploadPage.js
@@ -7,22 +7,26 @@ const UploadPage = () => {
   return ( 
     <section className='upload'>
       <h1 className='upload__title'>Upload Video</h1>
-      <div>
-        <p className='upload__label'>VIDEO THUMBNAIL</p>
-        <img className='upload__thumbnail' src={thumbnail} alt='video thumbnail'/>
-      </div>
       <form>
-        <div className='upload__input-container'>
-          <label className='upload__label' htmlFor='title'>
-            TITLE YOUR VIDEO
-          </label>
-          <input className='upload__input' name='title' placeholder='Add a title to your video'/>
-        </div>
-        <div className='upload__input-container upload__input-container--border'>
-          <label className='upload__label' htmlFor='describe'>
-            ADD A VIDEO DESCRIPTION
-          </label>
-          <input className='upload__input upload__input--describe' name='describe' placeholder='Add a description to your video'/>
+        <div className='upload__form-container'>
+          <div className='upload__image-container'>
+            <p className='upload__label'>VIDEO THUMBNAIL</p>
+            <img className='upload__thumbnail' src={thumbnail} alt='video thumbnail'/>
+          </div>
+          <div className='upload__input-container--display'>
+          <div className='upload__input-container'>
+            <label className='upload__label' htmlFor='title'>
+              TITLE YOUR VIDEO
+            </label>
+            <input className='upload__input' name='title' placeholder='Add a title to your video'/>
+          </div>
+          <div className='upload__input-container upload__input-container--border'>
+            <label className='upload__label' htmlFor='describe'>
+              ADD A VIDEO DESCRIPTION
+            </label>
+            <input className='upload__input upload__input--describe' name='describe' placeholder='Add a description to your video'/>
+          </div>
+          </div>
         </div>
         <div className='upload__button-container'>
           <Link to={'/'}>
